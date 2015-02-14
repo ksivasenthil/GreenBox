ad576b24fbe5e76984793581998e2c1414a3efa0
K.Siva Senthil <ksivasenthil@gmail.com>
2015-02-15 05:10:00 +0530

    Modified DB columns for FuelType
    Added Heating Value and renamed he CO2Emissions to CO2EmissionFactor.
    This is to accommodate for the new formula for calculation of CO2
    emissions using the fuel type method.

--- Main/Solution/Storage/Structure/GreenBox/dbo/Tables/FuelType.sql
+++ Main/Solution/Storage/Structure/GreenBox/dbo/Tables/FuelType.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[FuelType] (
     [Id]           UNIQUEIDENTIFIER NOT NULL,
     [Description]  VARCHAR (100)    NOT NULL,
-    [CO2Emissions] DECIMAL (8, 2)   NOT NULL,
+	[HeatingValue] DECIMAL (8,2) NOT NULL,
+    [CO2EmissionFactor] DECIMAL (8, 2)   NOT NULL,
     [ErrorMargin]  DECIMAL (4, 3)   NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
