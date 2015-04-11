4fc5ca2ee50afc70845def7a51d28c5f18a967ca
K.Siva Senthil <ksivasenthil@gmail.com>
2015-04-11 21:28:14 +0530

    Adding the missed out file
    Adding the missed out script file.

--- Main/Solution/Site/Scripts/ApiCaller.js
+++ Main/Solution/Site/Scripts/ApiCaller.js
@@ -1,9 +1,49 @@
-﻿$(document).ready(function () {
-    var vechileTypes = $('#vt');
-    $.getJSON("/api/GreenBoxApi/GetAllVehicleTypes").
-        done(function (data) {
-            $.each(data, function (key, item) {
-                alert("Value retrieved: Index - " + key + " with id : " + item.Id + " and value : "+ item.Description);
+﻿var pageDataHandler = {
+    'opt': function (src, data, defaultValue, defaultText) {
+        var otControl = $(src);
+        var otVehicleTypes = $('#optionsTemplate').html();
+        var optionData = {
+            "options": []
+        };
+        var formattedMarkup = null;
+
+        optionData.options.push({
+            "key": defaultValue,
+            "text": defaultText
+        });
+        $.each(data, function (key, item) {
+            optionData.options.push({
+                "key": item.Id,
+                "text": item.Description
             });
         });
+        formattedMarkup = Mustache.render(otVehicleTypes, optionData);
+        otControl.html(formattedMarkup);
+    },
+    'vt': function () {
+        $.getJSON("/api/GreenBoxApi/GetAllVehicleTypes").
+            done(function (data) {
+                pageDataHandler.opt('#vt', data, 0, 'Select Vehicle Type');
+            });
+    },
+    'svt': function (vtId) {
+        $.getJSON("/api/GreenBoxApi/GetVehicleSubTypeById/" + vtId).
+            done(function (data) {
+                pageDataHandler.opt('#svt', data, 0, 'Select Vehicle Sub Type');
+            });
+    },
+    'gb': function (result, distance, subType, successCallBack) {
+        distance = distance.replace(/~$/, "");
+        $.getJSON("/api/GreenBoxApi/GetEmissionFactors?data=" + subType + ',<root></root>,' + distance).
+            done(function (data) {
+                successCallBack(result, data);
+            });
+    }
+};
+$(document).ready(function () {
+    pageDataHandler.vt();
+    $('#vt').change(function () {
+        pageDataHandler.svt($(this).val());
+    });
+
 });
